ecc798fd4fbf330c7fafa2202735eb3b73adc7bd
Xiaojie Zheng <xz1935@nyu.edu>
2018-12-16 19:45:26 -0500

    add image upload

---
+++ www/getpetowneruserinfo.php
@@ -0,0 +1,31 @@
+<?php
+{
+$servername = "localhost";
+$dbusername = "websysF182";
+$dbpassword = "websysF182!!";
+$dbname = "websysF182";
+
+session_start();
+$EMAIL_ID = $_SESSION["USER_ID"];
+$link = mysql_connect($servername,$dbusername,$dbpassword) or die('Cannot connect to the database.');
+mysql_select_db($dbname,$link) or die('Cannot select database.');
+	
+//get pet information for pet lover homepage
+$query = "SELECT EMAIL_ID,FNAME,LNAME,PHONE,GENDER,DOB,LOCATION FROM PET_OWNER WHERE EMAIL_ID =\"$EMAIL_ID\"";
+$result = mysql_query($query,$link) or die('Errant query: '.$query);
+
+/* create one master array of the records*/
+$users = array();
+if(mysql_num_rows($result)){
+	while($user = mysql_fetch_assoc($result)){
+		$users[] = array('user'=>$user);
+	}
+}
+
+/*output in necessary format*/
+header('Content-type:application/json');
+echo json_encode(array('users'=>$users));
+
+@mysql_close($link);
+}
+?>
